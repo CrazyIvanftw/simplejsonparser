49c8a9bf49b7cc38215b1f00e5ce987045136a63
Simon Farre <dat14sfa@student.lu.se>
2017-02-04 00:06:05 +0100

    Testing multiple branches, this is the 1st dev branch for the json_decoder. This project is *not* meant to go anywhere, just only meant to test out, and learn github thingies

--- json_decode.h
+++ json_decode.h
@@ -1,7 +1,22 @@
 #ifndef JSON_DECODE_H
 #define JSON_DECODE_H
 
+
+// adding comments to test multiple branches, along with some basic skeleton code, and basic
+// includes
+#include <string>
+#include <vector>
+#include <utility>
 class json_decode {
+	public:
+		
+	json_decode();
+	~json_decode();
+
+	json_decode(std::string&);
+	
+	template <typename ...Output, typename O1>
+	auto decode(std::string&) -> decltype(std::declval<O1>());
 
 };
 
